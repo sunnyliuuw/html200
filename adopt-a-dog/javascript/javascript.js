62385442239db067706e6650b02650eb6c488a22
Shitong Liu <sunnyliuuw@gmail.com>
2019-03-25 00:00:27 -0700

    add jQuery use CDN, add hover effect using jquery

--- adopt-a-dog/javascript/javascript.js
+++ adopt-a-dog/javascript/javascript.js
@@ -23,45 +23,7 @@ function hasName(name) {
   return false;
 }
 
-document.getElementById('totalCost').innerHTML = totalCost;
-
-//Form starts
-const container = document.getElementById('divBox');
-const form = document.createElement('form');
-form.setAttribute('class', 'contact-us')
-const fieldId = ['firstname', 'lastname', 'email', 'comments'];
-const fieldLabel = ['First Name', 'Last Name', 'Email Address', 'Comments'];
-const fieldType = ['text', 'text', 'email', 'text'];
-
-for (let i = 0; i < fieldId.length; i++) {
-  const label = document.createElement('label');
-  label.textContent = fieldLabel[i];
-  label.setAttribute('for', fieldId[i]);
-
-  const input = document.createElement('input');
-  input.setAttribute('id', fieldId[i]);
-  input.setAttribute('type', fieldType[i]);
-  input.setAttribute('required', true);
-
-  form.appendChild(label);
-  form.appendChild(input);
-}
-
-const submitButton = document.createElement('button');
-submitButton.setAttribute('type', 'submit');
-submitButton.textContent = 'Submit';
-
-form.appendChild(submitButton);
-
-form.addEventListener('submit', function(e) {
-  e.preventDefault();
-  alert('Your submission has been received');
-  for (let j = 0; j < form.elements.length-1; j++) {
-    console.log(fieldLabel[j] + ' : ' + form.elements[j].value);
-  }
-});
-
-container.appendChild(form);
-
-console.log(container);
-//form ends
+$('.dog-profile').hover(
+  function(){ $(this).toggleClass('dog-border') },
+  function(){ $(this).toggleClass('dog-border') }
+);
